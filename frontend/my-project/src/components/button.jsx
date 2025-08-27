f974cf570f2614525a8eefd87d5a0ade5afe90d5
razieh i <izanloo@amwajco.net>
2025-08-27 17:41:37 +0330

    create button component & add style for it

--- frontend/my-project/src/components/button.jsx
+++ frontend/my-project/src/components/button.jsx
@@ -1,19 +1,19 @@
-function Button({
+import "./button.scss";
+
+export const Button = ({
   children,
   onClick,
   variant = "primary",
   className,
   ...rest
-}) {
+}) => {
   return (
     <button
       onClick={onClick}
-      className={`btn btn--${variant} ${className}`}
+      className={`btn--${variant} ${className}`}
       {...rest}
     >
       {children}
     </button>
   );
-}
-
-export default Button;
+};
