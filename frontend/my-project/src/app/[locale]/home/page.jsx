e60f19bca4996900ce26665135aca2f7a7d5f70c
razieh i <izanloo@amwajco.net>
2025-08-30 14:22:22 +0330

    change url 'button component'

--- frontend/my-project/src/app/[locale]/home/page.jsx
+++ frontend/my-project/src/app/[locale]/home/page.jsx
@@ -1,5 +1,5 @@
 import { serverSideTranslation } from "lib/i18n/initTranslations";
-import { Button } from "@/components/button";
+import { Button } from "@/components/button/button";
 import Link from "next/link";
 import "./page.scss";
 
