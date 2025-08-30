afeb7d2a832fa0d401f80fa7e8ecf649242de703
razieh i <izanloo@amwajco.net>
2025-08-30 14:12:34 +0330

    use i18next in middleware

--- frontend/my-project/src/middleware.js
+++ frontend/my-project/src/middleware.js
@@ -1,8 +1,16 @@
 import { i18nRouter } from "next-i18n-router";
 import { i18nConfig } from "./lib/i18n/i18n";
+import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  return i18nRouter(request, i18nConfig);
+  const { pathname } = request.nextUrl;
+  const i18nRouterValue = i18nRouter(request, i18nConfig);
+  const locale = i18nRouterValue.headers.get("x-next-i18n-router-locale");
+
+  if (pathname === "/")
+    return NextResponse.redirect(new URL(`${locale}/home`, request.url));
+
+  return i18nRouterValue;
 }
 
 export const config = {
