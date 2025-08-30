15a8dd0ea2339dcbd5af16a68fcb10a664c4f2ba
razieh i <izanloo@amwajco.net>
2025-08-30 14:11:30 +0330

    config: add page home

---
+++ frontend/my-project/src/app/[locale]/home/page.jsx
@@ -0,0 +1,40 @@
+import { serverSideTranslation } from "lib/i18n/initTranslations";
+import { Button } from "@/components/button";
+import Link from "next/link";
+import "./page.scss";
+
+const HomePage = async ({ params }) => {
+  const { locale } = await params;
+  const { t } = await serverSideTranslation(locale, ["home"]);
+
+  const btnsLink = [
+    {
+      title: "studyBlog",
+      url: "blogs",
+      variant: "outline",
+    },
+    {
+      title: "manageBlog",
+      url: "profile",
+      variant: "primary",
+    },
+  ];
+
+  return (
+    <div className="home-page">
+      <h1>{t("title")}</h1>
+      <div className="d-flex flex-column align-items-center px-2">
+        <p>{t("description")}</p>
+        <div className="d-flex justify-content-center mt-4 gap-4">
+          {btnsLink.map((item) => (
+            <Button variant={item.variant} key={item.url}>
+              <Link href={`${locale}/${item.url}`}>{t(item.title)}</Link>
+            </Button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- frontend/my-project/src/app/page.jsx
+++ frontend/my-project/src/app/page.jsx
@@ -1,28 +1,9 @@
-import { Button } from "@/components/button";
-import Link from "next/link";
-import "./page.scss";
+import HomePage from "./[locale]/home/page";
 
-const HomePage = () => {
+const Home = () => {
   return (
-    <div className="home-page">
-      <h1>اپلیکیشن مدیریت بلاگ</h1>
-
-      <div className="d-flex flex-column align-items-center px-2">
-        <p>
-          جایی که قراره بتونی یه اپلیکیشن بلاگ کامل رو مدیریت کنی! بتونی بلاگ
-          بسازی - کامنت بگذاری و در پنلت همه اتفاقات رو رصد کنی!
-        </p>
-        <div className="d-flex justify-content-center mt-4 gap-4">
-          <Button variant="outline">
-            <Link href="/blogs">مطالعه بلاگ ها</Link>
-          </Button>
-          <Button variant="primary">
-            <Link href="/profile">مدیریت بلاگ ها</Link>
-          </Button>
-        </div>
-      </div>
-    </div>
+   <HomePage/>
   );
 };
 
-export default HomePage;
+export default Home;
