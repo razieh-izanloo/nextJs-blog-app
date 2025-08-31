978d2de8d92087e488103b1f41b556e5299153db
razieh i <izanloo@amwajco.net>
2025-08-31 13:26:23 +0330

    set locale in cookie & use it in client-component

--- frontend/my-project/src/middleware.js
+++ frontend/my-project/src/middleware.js
@@ -6,9 +6,14 @@ export function middleware(request) {
   const { pathname } = request.nextUrl;
   const i18nRouterValue = i18nRouter(request, i18nConfig);
   const locale = i18nRouterValue.headers.get("x-next-i18n-router-locale");
+  const response = NextResponse.next();
 
+  // for use locale in client-component
+  const cookieLocale = request.cookies.get("NEXT_LOCALE");
+  if (cookieLocale !== locale) response.cookies.set("NEXT_LOCALE", locale);
+console.log("loaaaaaad", locale)
   if (pathname === "/")
-    return NextResponse.redirect(new URL(`${locale}/home`, request.url));
+    return NextResponse.redirect(new URL("/home", request.url));
 
   return i18nRouterValue;
 }
