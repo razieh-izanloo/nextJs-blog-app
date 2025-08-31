53669f5196678a2a999d02c1a01d3ec86b05255d
razieh i <izanloo@amwajco.net>
2025-08-31 13:23:32 +0330

    feat: create 'langaugeSelector' & use in header

---
+++ frontend/my-project/src/components/header/index.jsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { NavLink } from "./navLink";
+// import { useAuth } from "@/context/AuthContext";
+import { LanguageSelector } from "@/components/languageSelector/languageSelector";
+import "./index.scss";
+import { useClientSideTranslation } from "@/lib/i18n/useClientSideTranslation";
+import Skeleton from "react-loading-skeleton";
+
+export const Header = (props) => {
+  //   const { user, isLoading } = useAuth();
+  const { locale } = props;
+  let user = false;
+  const isLoading = false;
+  const { t } = useClientSideTranslation(locale, ["header"]);
+  console.log(t);
+  const navLinks = [
+    {
+      id: 1,
+      label: "home",
+      path: "/",
+    },
+    {
+      id: 2,
+      label: "blogs",
+      path: "/blogs",
+    },
+  ];
+
+  return (
+    <header className={isLoading ? "isLoading" : ""}>
+      <nav className="w-100">
+        <ul className="d-flex align-items-center justify-content-between p-3">
+          <LanguageSelector />
+          <div className="d-flex align-items-center gap-3">
+            {navLinks.map((navLink) => {
+              return (
+                <li key={navLink.id}>
+                  <NavLink path={navLink.path}>
+                    {t ? t(navLink.label) : <Skeleton width="20px" />}
+                  </NavLink>
+                </li>
+              );
+            })}
+            <li>
+              {user ? (
+                <NavLink path="/profile">
+                  {t ? t("profile") : <Skeleton width="20px" />}
+                </NavLink>
+              ) : (
+                <NavLink path="/signin">
+                  {t ? t("signin") : <Skeleton width="20px" />}
+                </NavLink>
+              )}
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+};
---
+++ frontend/my-project/src/components/languageSelector/languageSelector.jsx
@@ -0,0 +1,76 @@
+"use client";
+import { infoLang } from "@/lib/i18n/infoLang";
+// import { useAppSelector } from "@/redux/hooks";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import "./languageSelector.scss";
+import { useGetCurrentLang } from "hooks/useGetCurrentLang";
+
+export const LanguageSelector = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentLang, setCurrentLang] = useState("fa");
+  const pathname = usePathname();
+
+  const updateUrl = (lang) => {
+    const segments = pathname.split("/");
+    segments[segments[1] === currentLang ? 1 : 0] = lang;
+    return segments.join("/");
+  };
+
+  const selectLanguage = (lang) => {
+    setIsOpen(false);
+    setCurrentLang(lang)
+  };
+
+  const initialCurrentLang = useGetCurrentLang();
+  useEffect(() => {
+    if (initialCurrentLang !== currentLang) setCurrentLang(initialCurrentLang);
+  }, [initialCurrentLang]);
+
+console.log(currentLang)
+  return (
+    <div className="position-relative inline-block section-languageSelector">
+      <button onClick={() => setIsOpen(!isOpen)}>
+        <Image
+          src={`/images/flags/${currentLang}.svg`}
+          alt={""}
+          width={24}
+          height={24}
+        />
+        <span className="current-lang">{infoLang[currentLang].label}</span>
+        <Image
+          src="/images/icons/arrow.svg"
+          alt="change language"
+          width={10}
+          height={10}
+          className="icon-arrow"
+        />
+      </button>
+
+      {isOpen && (
+        <div className="isOpen">
+          {["fa", "en"].map((lang) => (
+            <Link
+              href={updateUrl(lang)}
+              key={lang}
+              onClick={() => selectLanguage(lang)}
+              className={`link-lang
+                 ${lang === currentLang ? "current-link" : ""}`}
+            >
+              <Image
+                src={`/images/flags/${lang}.svg`}
+                alt={""}
+                width={24}
+                height={24}
+              />
+
+              {infoLang[lang].label}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
