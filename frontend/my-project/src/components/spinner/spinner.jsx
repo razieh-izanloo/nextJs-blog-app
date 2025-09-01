0cd3197baf4626b35784f26a873051d3e17f73b0
razieh i <izanloo@amwajco.net>
2025-09-01 15:37:39 +0330

    feat: create spiiner for loading

---
+++ frontend/my-project/src/components/spinner/spinner.jsx
@@ -0,0 +1,13 @@
+import "./spinner.scss";
+
+export const Spinner = () => {
+  return (
+    <div className="w-100 d-flex justify-content-center">
+      <div class="lds-roller">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div key={index}></div>
+        ))}
+      </div>
+    </div>
+  );
+};
