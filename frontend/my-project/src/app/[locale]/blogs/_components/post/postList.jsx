fb2e366928e8dcfe279eae0c119939666f05139a
razieh i <izanloo@amwajco.net>
2025-09-03 12:05:00 +0330

    optimal: add func 'getPosts' & use it in postList

--- frontend/my-project/src/app/[locale]/blogs/_components/post/postList.jsx
+++ frontend/my-project/src/app/[locale]/blogs/_components/post/postList.jsx
@@ -1,18 +1,15 @@
+import { getPosts } from "@/services/postServices";
 import { CardPost } from "./cardPost";
 
 export const PostList = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
-
-  const {
-    data: { posts },
-  } = await res.json();
+  const posts = await getPosts();
 
   return (
     <>
       {posts?.length > 0 ? (
         <div className="row mx-0">
           {posts.map((item) => (
-            <CardPost key={item._id} item={item}/>
+            <CardPost key={item._id} item={item} />
           ))}
         </div>
       ) : null}
