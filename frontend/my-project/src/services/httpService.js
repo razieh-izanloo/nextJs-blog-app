11d95db5b950907c7ccf9d0649a8b85e467518c4
razieh i <izanloo@amwajco.net>
2025-09-09 16:19:43 +0330

    feat: add 'app.interseptors' for auth user

--- frontend/my-project/src/services/httpService.js
+++ frontend/my-project/src/services/httpService.js
@@ -5,6 +5,33 @@ const app = axios.create({
   withCredentials: true,
 });
 
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+//user/profile => 401
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    originalConfig._retry = true;
+    if (err.response.status === 401 && !originalConfig) {
+      try {
+        const { data } = await axios.get(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/user/refresh-token`,{
+            withCredentials: true
+          }
+        );
+        if (data) return app(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 const http = {
   get: app.get,
   patch: app.patch,
