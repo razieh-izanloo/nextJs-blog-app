fadeca0295b9095e5c7f29100a4e795b12581f2f
razieh i <izanloo@amwajco.net>
2025-09-13 16:43:47 +0330

    use searchParams

--- frontend/my-project/src/app/blogs/(postList)/category/[categorySlug]/page.jsx
+++ frontend/my-project/src/app/blogs/(postList)/category/[categorySlug]/page.jsx
@@ -1,23 +1,30 @@
+import queryString from "query-string";
 import { PostList } from "app/blogs/_components/post/postList";
+import { setCookieOnReq } from "@/utils/setCookieOnReq";
+import { getPosts } from "@/services/postServices";
+import { cookies } from "next/headers";
 
-const Category = async ({ params }) => {
+const Category = async ({ params, searchParams }) => {
   const { categorySlug } = params;
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/post/list?categorySlug=${categorySlug}`
-  );
+  const queries = `${queryString.stringify(
+    searchParams
+  )}&categorySlug=${categorySlug}`;
 
-  const { data } = await res.json();
-  const { posts } = data || {};
+  const cookieStore = cookies();
+  const options = setCookieOnReq(cookieStore);
+  const posts = await getPosts(queries, options);
 
   return (
-    <>
+    <div>
       {posts.length === 0 ? (
-        <p className="fs-5">پستی در این دسته بندی پیدا نشد</p>
+        <p className="text-lg text-secondary-600 ">
+          پستی در این دسته بندی پیدا نشد
+        </p>
       ) : (
         <PostList posts={posts} />
       )}
-    </>
+    </div>
   );
 };
 export default Category;
--- frontend/my-project/src/app/blogs/(postList)/page.jsx
+++ frontend/my-project/src/app/blogs/(postList)/page.jsx
@@ -1,26 +1,27 @@
+import queryString from "query-string";
 import { cookies } from "next/headers";
 import { PostList } from "../_components/post/postList";
 import { getPosts } from "@/services/postServices";
 import { setCookieOnReq } from "@/utils/setCookieOnReq";
 
-const BlogPage = async () => {
+const BlogPage = async ({ searchParams }) => {
+  const queries = queryString.stringify(searchParams);
   const cookieStore = cookies();
   const options = setCookieOnReq(cookieStore);
-  const posts = await getPosts(options);
+  const posts = await getPosts(queries, options);
+
+  const { search } = searchParams;
+
   return (
     <>
-      <p>
-        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده
-        از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
-        سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
-        متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه
-        درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با
-        نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
-        خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید
-        داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان
-        رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
-        پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-      </p>
+      {search ? (
+        <p className="mb-4 text-secondary-700">
+          {posts.length === 0
+            ? " هیچ پستی با این مشخصات پیدا نشد "
+            : `نشان دادن ${posts.length} نتیجه برای`}
+          <span className="font-bold">&quot;{search}&quot;</span>
+        </p>
+      ) : null}
       <PostList posts={posts} />
     </>
   );
