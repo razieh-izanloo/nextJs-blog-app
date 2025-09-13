1a7ed72f268e529b31a5866fec546210e873263a
razieh i <izanloo@amwajco.net>
2025-09-13 16:44:42 +0330

    add queries to api(post/list)

--- frontend/my-project/src/services/postServices.js
+++ frontend/my-project/src/services/postServices.js
@@ -10,9 +10,9 @@ export async function getPostBySlug(slug) {
   return post;
 }
 
-export async function getPosts(options) {
+export async function getPosts(queries, options) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/post/list`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/post/list?${queries}`,
     options
   );
 
