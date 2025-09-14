fbfb40d17729ab6fdfae50f81025d959f6dee944
razieh i <izanloo@amwajco.net>
2025-09-14 12:34:43 +0330

    feat: create submitButton component

---
+++ frontend/my-project/src/components/submitButton/submitButton.jsx
@@ -0,0 +1,19 @@
+import { useFormStatus } from "react-dom";
+import SvgLoaderComponent from "./SVGLoaderComponent";
+import { Button } from "@/components/button/button";
+
+export const SubmitButton = ({ children, calssName, ...rest }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      disabled={pending}
+      {...rest}
+      className={`d-flex align-items-center justify-content-center gap-4 py-4 w-100
+        ${calssName}
+        `}
+    >
+      {children}
+      {pending && <SvgLoaderComponent />}
+    </Button>
+  );
+};
