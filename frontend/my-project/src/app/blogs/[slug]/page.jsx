d2babc2ea70d8bc18265a0a92b0aa9bcafe22d15
razieh i <izanloo@amwajco.net>
2025-09-14 12:36:43 +0330

    use component postComponent

--- frontend/my-project/src/app/blogs/[slug]/page.jsx
+++ frontend/my-project/src/app/blogs/[slug]/page.jsx
@@ -3,24 +3,28 @@ import { notFound } from "next/navigation";
 import { getPostBySlug, getPosts } from "@/services/postServices";
 import { RelatedPost } from "../_components/RelatedPost";
 import "./page.scss";
+import { PostComment } from "../_components/comment/postComment";
 
 export const dynamicParams = false;
 
-export async function generateStaticParams(){
+export async function generateStaticParams() {
   const posts = await getPosts();
-  const slugs = posts.map(post => ({slug: post.slug}))
+  const slugs = posts.map((post) => ({ slug: post.slug }));
   return slugs;
 }
 
 export async function generateMetadata({ params }) {
-  const post = await getPostBySlug(params.slug);
+  const { slug } = await params;
+  const post = await getPostBySlug(slug);
   return {
     title: `پست ${post.title}`,
   };
-} 
+}
 
 const SinglePost = async ({ params }) => {
-  const post = await getPostBySlug(params.slug);
+  const { slug } = await params;
+
+  const post = await getPostBySlug(slug);
 
   if (!post) notFound();
 
@@ -33,8 +37,8 @@ const SinglePost = async ({ params }) => {
         <div className="img-box">
           <Image fill src={post.coverImageUrl} alt={post.title} />
         </div>
-        {post.related.length > 0 && <RelatedPost posts={post.related} />} 
-         {/* <PostComment post={post} /> */}
+        {post.related.length > 0 && <RelatedPost posts={post.related} />}
+        <PostComment post={post} />
       </div>
     </div>
   );
