c369d3a06e8c1b22b05c193cfe0e9e7dcc86cbca
razieh i <izanloo@amwajco.net>
2025-09-21 16:48:15 +0330

    add suspens

--- frontend/my-project/src/app/(dashboard)/profile/page.jsx
+++ frontend/my-project/src/app/(dashboard)/profile/page.jsx
@@ -1,21 +1,24 @@
-import { fetchHardData } from "@/services/data";
-import { Card } from "./_components/card";
+import { Suspense } from "react";
+import { CardWarpper } from "./_components/cardWrapper";
+import { PostTable } from "./posts/_/components/postTable";
+import { Spinner } from "@/components/spinner/spinner";
 import "./page.scss";
 
-const ProfilePage = async () => {
-  const { numberOfUsers, numberOfComments, numberOfPosts } =
-    await fetchHardData();
-
+const ProfilePage = () => {
   return (
-
+    <>
       <div className="container">
-        <div className="row gx-2">
-          <Card title="کاربران" value={numberOfUsers} type="users" />
-          <Card title="پست ها" value={numberOfPosts} type="posts" />
-          <Card title="نظرات" value={numberOfComments} type="comments" />
-        </div>
-      </div>
+        <h1>داشبورد</h1>
+        <Suspense fallback={<Spinner />}>
+          <CardWarpper />
+        </Suspense>
+        <h2 className="mt-4">آخرین پست ها</h2>
 
+        <Suspense fallback={<Spinner />}>
+          <PostTable query="sort=latest&limit=5" />
+        </Suspense>
+      </div>
+    </>
   );
 };
 
--- frontend/my-project/src/app/(dashboard)/profile/posts/page.jsx
+++ frontend/my-project/src/app/(dashboard)/profile/posts/page.jsx
@@ -1,6 +1,25 @@
+import { Suspense } from "react";
 import { PostTable } from "./_/components/postTable";
+import { Spinner } from "@/components/spinner/spinner";
+import { Search } from "@/components/search/search";
+import { CreatePost } from "./_/components/buttons";
+import "./page.scss";
+import queryString from "query-string";
 
-const PostsPage = ()=>{
-    return(<><PostTable/></>)
-}
+const PostsPage = ({ searchParams }) => {
+  const query = queryString.stringify(searchParams);
+
+  return (
+    <div className="container">
+      <div className="row row-cols-md-3">
+        <h2>لیست پست ها</h2>
+        <Search />
+        <CreatePost />
+      </div>
+      <Suspense fallback={<Spinner />} key={query}>
+        <PostTable query={query} />
+      </Suspense>
+    </div>
+  );
+};
 export default PostsPage;
