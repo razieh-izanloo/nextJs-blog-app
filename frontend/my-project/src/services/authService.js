5cb725bb8f69afdbb39ff0fcfc13f709f19ade77
razieh i <izanloo@amwajco.net>
2025-09-21 15:36:07 +0330

    create 'getAllUsersApi'

--- frontend/my-project/src/services/authService.js
+++ frontend/my-project/src/services/authService.js
@@ -1,13 +1,18 @@
 import http from "./httpService";
 
-export const signupApi = async(data) => {
+export const signupApi = async (data) => {
   return http.post("/user/signup", data).then(({ data }) => data.data);
 };
 
-export const signinApi = async(data) => {
+export const signinApi = async (data) => {
   return http.post("/user/signin", data).then(({ data }) => data.data);
 };
 
-export const getUserApi = async(data) => {
+export const getUserApi = async (data) => {
   return http.get("/user/profile", data).then(({ data }) => data.data);
 };
+
+export const getAllUsersApi = async (data, option) => {
+  return http.get("/user/list", data, option).then(({ data }) => data.data);
+};
+
