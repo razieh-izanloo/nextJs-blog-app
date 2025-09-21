5d0830a43b7067ce371181cf3c3883231f2fd209
razieh i <izanloo@amwajco.net>
2025-09-21 15:35:48 +0330

    create 'getAllCommentsApi'

--- frontend/my-project/src/services/commentService.js
+++ frontend/my-project/src/services/commentService.js
@@ -3,3 +3,7 @@ import http from "./httpService";
 export const createCommentApi = async (data, options) => {
   return http.post("/comment/add", data, options).then(({ data }) => data.data);
 };
+
+export const getAllCommentsApi = async (options = {}) => {
+  return http.get("/comment/list", options).then(({ data }) => data.data);
+};
